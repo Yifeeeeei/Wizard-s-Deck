35203080a556ecb0fcb97319746e54203ec29072
if <55980773+Yifeeeeei@users.noreply.github.com>
2023-08-10 00:41:22 +0800

    added border done

--- card_maker.py
+++ card_maker.py
@@ -17,6 +17,8 @@ class Config:
         self.drawing_width = 510
         self.drawing_height = 510
         self.drawing_to_upper = 40
+        self.border_width = 560
+        self.border_height = 830
         self.general_path = ""  # 通用素材
         self.drawing_path = ""  # 卡牌原画
         self.font_path = ""  # 字体
@@ -103,55 +105,64 @@ class CardMaker:
         image = image.resize((target_width, target_height))
         return image
 
-        if width == card_width and height == card_height:
-            return image
-        if width >= card_width and height >= card_height:
-            left = (width - card_width) / 2
-            top = (height - card_height) / 2
-            right = (width + card_width) / 2
-            bottom = (height + card_height) / 2
-            image = image.crop((left, top, right, bottom))
-        else:
-            image = image.resize((card_width, card_height))
-        return image
-
-    def get_drawing(self, card_info: CardInfo):
+    def get_image_without_extension(self, image_name):
         extension_list = [".png", ".jpg", ".jpeg"]
         for ext in extension_list:
-            if os.path.exists(
-                os.path.join(self.config.drawing_path, card_info.name + ext)
-            ):
-                raw_img = PIL.Image.open(
-                    os.path.join(self.config.drawing_path, card_info.name + ext)
-                )
-                return self.adjust_image(
-                    raw_img, (self.config.drawing_width, self.config.drawing_height)
-                )
-
-        print("could not find drawing for card: " + card_info.name)
+            if os.path.exists(image_name + ext):
+                return PIL.Image.open(image_name + ext).convert("RGBA")
+        print("could not find image: " + image_name)
         return None
 
+    def get_drawing(self, card_info: CardInfo):
+        return self.get_image_without_extension(
+            os.path.join(self.config.drawing_path, card_info.name)
+        )
+
     def get_background(self, card_info: CardInfo):
-        bg_image = PIL.Image.open(
+        bg_image = self.get_image_without_extension(
             os.path.join(
-                self.config.general_path,
-                "back_" + self.translator(card_info.category) + ".png",
+                self.config.general_path, "back_" + self.translator(card_info.category)
             )
-        ).convert("RGBA")
-        return self.adjust_image(
+        )
+        bg_image = self.adjust_image(
             bg_image, (self.config.card_width, self.config.card_height)
         )
+        return bg_image
+
+    def get_border(self):
+        border_image = self.get_image_without_extension(
+            os.path.join(self.config.general_path, "border")
+        )
+        border_image = self.adjust_image(
+            border_image, (self.config.border_width, self.config.border_height)
+        )
+        return border_image
 
     # 准备好底板，除了描述和血量，元素消耗之类的其他东西，包括原画
     def prepare_outline(self, card_info: CardInfo):
+        # 添加背景
         base_image = self.get_background(card_info)
+        # 添加卡牌原画
         drawing_image = self.get_drawing(card_info)
+        # 添加原画
         base_image.paste(
             drawing_image,
             (
                 int((self.config.card_width - self.config.drawing_width) / 2),
                 self.config.drawing_to_upper,
             ),
+        )
+        # 添加底部文字背景
+
+        # 获取边框
+        border_image = self.get_border()
+        base_image.paste(
+            border_image,
+            (
+                int((self.config.card_width - self.config.border_width) / 2),
+                int((self.config.card_height - self.config.border_height) / 2),
+            ),
+            mask=border_image,
         )
 
         # 添加边框
--- card_maker_test.py
+++ card_maker_test.py
@@ -4,42 +4,42 @@ import PIL.ImageFont
 import textwrap
 from card_maker import *
 
-# Load the base image
-base_image = PIL.Image.open(
-    "素材/Emma_Watson_as_a_powerful_mysterious_sorceress__summoning_lightning__d_S1600129147_St20_G7.5.jpeg"
-)
+# # Load the base image
+# base_image = PIL.Image.open(
+#     "素材/Emma_Watson_as_a_powerful_mysterious_sorceress__summoning_lightning__d_S1600129147_St20_G7.5.jpeg"
+# )
 
-card_width = 250
-card_height = 400
-
-
-# crop the base image from the center if it is too big, stretch it if it is too small
-def adjust_image(image):
-    width, height = image.size
-    if width >= card_width and height >= card_height:
-        left = (width - card_width) / 2
-        top = (height - card_height) / 2
-        right = (width + card_width) / 2
-        bottom = (height + card_height) / 2
-        image = image.crop((left, top, right, bottom))
-    else:
-        image = image.resize((card_width, card_height))
-    return image
+# card_width = 250
+# card_height = 400
 
 
-base_image = adjust_image(base_image)
+# # crop the base image from the center if it is too big, stretch it if it is too small
+# def adjust_image(image):
+#     width, height = image.size
+#     if width >= card_width and height >= card_height:
+#         left = (width - card_width) / 2
+#         top = (height - card_height) / 2
+#         right = (width + card_width) / 2
+#         bottom = (height + card_height) / 2
+#         image = image.crop((left, top, right, bottom))
+#     else:
+#         image = image.resize((card_width, card_height))
+# #     return image
 
 
-# Create a drawing object
-draw = PIL.ImageDraw.Draw(base_image)
-
-# Draw a circle
+# base_image = adjust_image(base_image)
 
 
-# Load the overlay image
-overlay_image = PIL.Image.open("素材/download (4).jpg")
+# # Create a drawing object
+# draw = PIL.ImageDraw.Draw(base_image)
 
-# Paste the overlay image on top of the base image
+# # Draw a circle
+
+
+# # Load the overlay image
+# overlay_image = PIL.Image.open("素材/download (4).jpg")
+
+# # Paste the overlay image on top of the base image
 # base_image.paste(overlay_image, (0, 0), overlay_image)
 # draw.ellipse((100, 100, 200, 200), fill=(255, 0, 0))
 
